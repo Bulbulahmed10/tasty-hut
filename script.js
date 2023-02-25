e22fd48e342e71675dedeedfe206a5d1777c35cf
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-02-25 10:11:41 +0600

    added show all functionality

--- script.js
+++ script.js
@@ -9,12 +9,55 @@ const loadData = async (searchFoodName) => {
   }
 };
 
+//! display meals
 const displayData = (meals) => {
   const foodContainer = document.getElementById("food-container");
+  const showAllBtn = document.getElementById("show-all-btn");
+  let firstTen = meals.slice(0, 9);
+
+  if (meals.length > firstTen.length) {
+    showAllBtn.style.display = "block";
+    showAllBtn.addEventListener("click", () => {
+      foodContainer.innerHTML = "";
+
+      meals.forEach((meal) => {
+        const div = document.createElement("div");
+        div.classList.add("item-card");
+        div.innerHTML = `
+             <div class="card card-side bg-base-100 shadow-xl h-[200px]">
+                 <figure class="w-[50%]">
+                   <img
+                     class="h-full object-cover"
+                     src=${meal.strMealThumb}
+                     alt="Movie"
+                   />
+                 </figure>
+                 <div class="w-[50%] p-2 flex flex-col items-start">
+                   <h4 class="capitalize font-bold mt-2">
+                     ${meal.strMeal}
+                   </h4>
+                   <p class="mt-2 h-[54%] overflow-hidden">
+                     ${meal.strInstructions}
+                   </p>
+                   <label for="my-modal-3" id="meal-details" onclick="mealDetails(${meal.idMeal})" class="mt-4 text-yellow-400 capitalize underline cursor-pointer">
+                     view details
+                   </label>
+                 </div>
+               </div>
+             `;
+        foodContainer.appendChild(div);
+      });
+      showAllBtn.style.display = "none";
+    });
+  } else {
+    showAllBtn.style.display = "none";
+  }
   foodContainer.innerHTML = "";
-  meals.forEach((meal) => {
+
+  firstTen.forEach((meal) => {
     const div = document.createElement("div");
     div.classList.add("item-card");
+
     div.innerHTML = `
         <div class="card card-side bg-base-100 shadow-xl h-[200px]">
             <figure class="w-[50%]">
@@ -41,12 +84,14 @@ const displayData = (meals) => {
   });
 };
 
+//! meal search functionality
 document.getElementById("search-btn").addEventListener("click", () => {
   const searchInput = document.getElementById("search-input");
   loadData(searchInput.value);
   searchInput.value = "";
 });
 
+//! meal details functionality
 const mealDetails = async (idMeal) => {
   const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
   try {
@@ -58,8 +103,8 @@ const mealDetails = async (idMeal) => {
   }
 };
 
+//! display meal information
 const displayMealDetails = (meal) => {
-  console.log(meal);
   document.getElementById("meal-name").innerText = meal.strMeal;
   document.getElementById("meal-img").src = meal.strMealThumb;
   document.getElementById("meal-category").innerText = meal.strCategory;
